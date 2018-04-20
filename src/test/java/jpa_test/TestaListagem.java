3f7844c0c0acc3e45d47594abbcf6242bfa5dbca
tomaslm <tomaslm@hotmail.com>
2018-04-19 21:05:03 -0300

    Initial commit

---
+++ src/test/java/jpa_test/TestaListagem.java
@@ -0,0 +1,26 @@
+package jpa_test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+	public static void main(String[] args) throws SQLException {
+		String stringConnection = "jdbc:postgresql://localhost:5432/loja_virtual";
+		Connection connection = DriverManager.getConnection(stringConnection, "tomas", "");
+		Statement statement = connection.createStatement();
+		statement.execute("select * from Produto");
+		ResultSet rs = statement.getResultSet();
+		while (rs.next()) {
+			String nome = rs.getString("nome");
+			String descricao = rs.getString("descricao");
+			Long id = rs.getLong("id");
+
+			System.out.println("Nome: " + nome + ", " + descricao + ", " + id);
+		}
+		rs.close();
+		connection.close();
+	}
+}
