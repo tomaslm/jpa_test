dbc1ed150e3364e71d8faba80104819024f0807e
tomaslm <tomaslm@hotmail.com>
2018-04-19 21:09:54 -0300

    Change to try with resources

--- src/test/java/jpa_test/TestaListagem.java
+++ src/test/java/jpa_test/TestaListagem.java
@@ -10,17 +10,19 @@ public class TestaListagem {
 	public static void main(String[] args) throws SQLException {
 		String stringConnection = "jdbc:postgresql://localhost:5432/loja_virtual";
 		Connection connection = DriverManager.getConnection(stringConnection, "tomas", "");
-		Statement statement = connection.createStatement();
-		statement.execute("select * from Produto");
-		ResultSet rs = statement.getResultSet();
-		while (rs.next()) {
-			String nome = rs.getString("nome");
-			String descricao = rs.getString("descricao");
-			Long id = rs.getLong("id");
+		try (Statement statement = connection.createStatement()) {
+			statement.execute("select * from Produto");
+			try (ResultSet rs = statement.getResultSet()) {
+				while (rs.next()) {
+					String nome = rs.getString("nome");
+					String descricao = rs.getString("descricao");
+					Long id = rs.getLong("id");
 
-			System.out.println("Nome: " + nome + ", " + descricao + ", " + id);
+					System.out.println("Nome: " + nome + ", " + descricao + ", " + id);
+				}
+			}
 		}
-		rs.close();
 		connection.close();
 	}
+
 }
